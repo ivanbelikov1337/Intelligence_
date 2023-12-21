5b3afbbb3c3498c003b902fcda8fad323a94e96b
ivanbelikov1337 <120462639+ivanbelikov1337@users.noreply.github.com>
2023-12-21 16:47:42 +0200

    fix avatar

--- componets/header/userModalNavigate/UserModalNavigate.tsx
+++ componets/header/userModalNavigate/UserModalNavigate.tsx
@@ -22,51 +22,41 @@ interface IProps {
 }
 
 const UserModalNavigate: FC<IProps> = ({email,switcherModal,setSwitcherModal}) => {
-
     const fetcher = (url: string, init?: RequestInit) => fetch(url, init).then(res => res.json())
     const {data, isLoading} = useSWR<IData>(`/api/users/${email}`, fetcher)
 
-
-    if  (!data) {
-        return (
-            <Triangle
-                height="4.5rem"
-                width="4.5rem"
-                color="#6A5E96"
-                ariaLabel="triangle-loading"
-                visible={true}
-            />
+    if (data ) {
+        return  (
+            <div className={styles.container}>
+                <Image onClick={() => setSwitcherModal((prev:boolean) => !prev)}
+                       src={data.avatar} alt="avatar" width={50}
+                       className={styles.avatar}
+                       height={50}/>
+                {switcherModal &&
+                    <div className={styles.blockModule}>
+                        <div onClick={() => setSwitcherModal((prev:boolean) => !prev)} className={styles.closeModal}>
+                            <VscChromeClose size={"2rem"}/>
+                        </div>
+                        <button onClick={() => signOut({callbackUrl: '/'})}
+                                className={styles.logout}>
+                            <span>Logout</span>
+                            <MdLogout size={"2rem"}/>
+                        </button>
+                        <Link href={""} className={styles.account}>
+                            <span>Account</span>
+                            <PiUserList size={"2rem"}/>
+                        </Link>
+                        <Link href={""} className={styles.setting}>
+                            <span>Setting</span>
+                            <IoSettingsOutline size={"2rem"}/>
+                        </Link>
+                    </div>
+                }
+            </div>
         )
     }
 
-    return (
-        <div className={styles.container}>
-            <Image onClick={() => setSwitcherModal((prev:boolean) => !prev)}
-                   src={data.avatar} alt="avatar" width={50}
-                   className={styles.avatar}
-                   height={50}/>
-            {switcherModal &&
-                <div className={styles.blockModule}>
-                    <div onClick={() => setSwitcherModal((prev:boolean) => !prev)} className={styles.closeModal}>
-                        <VscChromeClose size={"2rem"}/>
-                    </div>
-                    <button onClick={() => signOut({callbackUrl: '/'})}
-                            className={styles.logout}>
-                        <span>Logout</span>
-                        <MdLogout size={"2rem"}/>
-                    </button>
-                    <Link href={""} className={styles.account}>
-                        <span>Account</span>
-                        <PiUserList size={"2rem"}/>
-                    </Link>
-                    <Link href={""} className={styles.setting}>
-                        <span>Setting</span>
-                        <IoSettingsOutline size={"2rem"}/>
-                    </Link>
-                </div>
-            }
-        </div>
-    )
+
 }
 
 export default UserModalNavigate;
